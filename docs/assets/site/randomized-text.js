4a30cc50ba4576240db6293d7d6fef36da1e03cc
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2022-05-01 15:10:17 -0400

    MORE. RANDOM. TEXT!!!!!
    Lots of stuff taken from Skyrim's guards, Sheogorath, and some other places.
    
    :nexusmodsmug:

--- docs/assets/site/randomized-text.js
+++ docs/assets/site/randomized-text.js
@@ -9,22 +9,27 @@
 const possibilities_conditionalized = [
     [ {random: .001}, "Well, well, well. You must be perty lucky!" ],
     [ {random: .01}, "Your firstborn is Sheson's!" ],
-    [ {random: .1}, "STOP REFRESHING!!!" ],
+    [ {random: .01}, "And why not? Imagine how unbearably, how unutterably cold the universe would be if one were all alone." ], // https://www.mit.edu/people/dpolicar/writing/prose/text/thinkingMeat.html
+    [ {random: .075}, "STOP REFRESHING!!!" ],
     [ {time: [[3,00], [11,59]]}, "Good morning!" ],
     [ {time: [[12,00], [12,00]]}, "Good noon!" ],
     [ {time: [[12,01], [18,00]]}, "Good afternoon!" ],
     [ {time: [[18,00], [24,00]]}, "Good evening!" ],
-    [ {time: [[12,00], [15,00]]}, "<s>Friendly reminder that you should probably be sleping right now</s>" ],
-    [ {date: [[0,0,0], [11,11,2022]]}, "Starfield community beta when?"],
+    [ {time: [[0,00], [5,00]]}, "<s>Friendly reminder that you should probably be sleping right now</s>" ],
+    [ {date: [[0,0,0], [11,11,2022]], random: .25}, "Starfield community beta when?"],
     [ {random: .00001}, "WHAT IN OBLIVION?!!! <b>WHY ARE YOU SO LUCKY?!!</b>" ],
-    [ {random: .1}, "Nexus Mods is the best!"],
-    [ {random: .1}, "Greetings, Dragonborn"], //rdunlap
-    [ {random: .1}, "Welcome, Dovahkiin"], //rdunlap
-    [ {random: .1}, "War. War never changes."], //rdunlap
-    [ {random: .01}, "The Institute liked that"], //rdunlap
-    [ {random: .01}, "Do you get to the Cloud District very often?"], //rdunlap
-    [ {random: .1}, "By Azura, by Azura, by Azura! It's you! The Grand Champion!"] //Otellino
-
+    [ {random: .33}, "Nexus Mods is the best!"],
+    [ {random: .02}, "Greetings, Dragonborn."], //rdunlap
+    [ {random: .1}, "Welcome, Dovahkiin."], //rdunlap
+    [ {random: .05}, "War. War never changes."], //rdunlap
+    [ {random: .01}, "The Institute liked that."], //rdunlap
+    [ {random: .01}, "Do you get to the Cloud District very often?"],
+    [ {random: .1}, "By Azura, by Azura, by Azura! It's you! The Grand Champion!"], //Otellino
+    [ {random: .1}, "You should not be here, Mortal! Your life is forfeit. Your flesh is mine."], //Otellino
+    [ {random: .1}, "Psst. Hey, I know who you are. Hail Sithis."],
+    [ {random: .1}, "You wear the armor of the bear, my friend. A fine choice."],
+    [ {time: [[20,00], [24,00]]}, "Only burglars and vampires creep around after dark. So which are you?" ],
+    [ {time: [[0,00], [5,00]]}, "Only burglars and vampires creep around after dark. So which are you?" ]
 ];
 
 const possibilities_Generic = [
@@ -54,8 +59,58 @@ const possibilities_Generic = [
     "That coin-pouch getting heavy?", 
     "So rise up! <b>RIIIIIISE UP</b>, children of the Empire! <b>RIIIIIISE UP</b>, Stormcloaks!", 
     "We mug people! And wagons.", 
-    "By the rules of the street, I now own there."
-];
+    "By the rules of the street, I now own there.",
+    "I just make character and run amok.", //chuckseven1
+    "You a fan of grilled cheese?",
+    "Ever tried Vegimite? Stuff's bitter as all get-out.",
+    "Sup.", //Haelfire
+    "I stuffed that kitty cat so full of lead, we'll have to use it as a pencil instead.",
+    "May the Bluebird of Paradise roost in your nostril.",
+    "¡Hola!",
+    "So who made the machines? That's who we want to contact.", // https://www.mit.edu/people/dpolicar/writing/prose/text/thinkingMeat.html
+    "Hey, check out the <a href=\"https://octodex.github.com/\" target=\"_blank\" rel=\"noopener noreferrer\">Octodex</a>!",
+    "I used to be an adventurer like you. Then I took an arrow in the knee...",
+    "Wonderful! Remarkably well preserved too.",
+    "I thought adventurers were supposed to look tough?",
+    "Halt. You're under arrest for murder and conspiracy against the city of Markarth!",
+    "They say Ulfric Stormcloak murdered the high king... with his voice! Shouted him apart!",
+    "'Tis a wicked axe you wield there, friend. That blade looks sharp enough to cut through a god.",
+    "You know what? You're not worth the hassle. Go... be some other guard's problem.",
+    "My cousin's out fighting dragons, and what do I get? Guard duty.",
+    "Iron sword, huh? What are you killing, butterflies?",
+    "I mostly deal with petty thievery and drunken brawls. Been too long since we've had a good bandit raid.",
+    "Your shield... Dwarf-make, is it not? But yet it seems so much... more.",
+    "By Shor, is that... is that Azura's Star? How did you come to possess such a rare treasure?",
+    "Fine armor you've got there. Dwarven make, am I right?",
+    "Judging by your armor, I'd say you're an Imperial scout. If so, well met.",
+    "That Stormcloak armor's getting on my nerves...",
+    "Hey, you mix potions, right? Can you brew me an ale?",
+    "Don't suppose you'd enchant my sword? Dull old blade can barely cut butter.",
+    "In the ancient tongue, you are Dovahkiin - Dragonborn!",
+    "They say the College has been snooping around Saarthal. Mages in a burial crypt. No good can come of that...",
+    "I'm just looking for my spoon.",
+    "I caught a glimpse of that captured dragon. It's.. beautiful. In its own way.",
+    "Thalmor in the Ratway? What's next, spriggans in the Bee and Barb?",
+    "Now I remember - you're that new member of the Companions. So you, what - fetch the mead?",
+    "Ooh, ooh, what kind of message? A song? A summons? Wait, I know! A death threat written on the back of an Argonian concubine!",
+    "But more to the point. Do you - tiny, puny, expendable little mortal - actually think you can convince me to leave?",
+    "I'm not a warlock, but I can make you one.", // Copilot?!!
+    "Was it Molag? No, no... Little Tim, the toymaker's son? The ghost of King Lysandus? Or was it... Yes! Stanley, that talking grapefruit from Passwall.",
+    "Now you. You can call me Ann Marie. But only if you're partial to being flayed alive and having an angry immortal skip rope with your entrails!",
+    "Sheogorath, Daedric Prince of Madness. At your service.",
+    "I am a part of you, little mortal. I am a shadow in your subconscious, a blemish on your fragile little psyche. You know me. You just don't know it.",
+    "Now that's the real question, isn't it? Because honestly, how much time off could a demented daedra really need?",
+    "Let's make sure I'm not forgetting anything. Clothes? Check. Beard? Check! Luggage?<br />Luggage! Now where did I leave my luggage?",
+    "Ha! I do love it when the mortals know they're being manipulated. Makes things infinitely more interesting.",
+    "The Wabbajack! Huh? Huh? Didn't see that coming, did you?",
+    "Well, I suppose it's back to the Shivering Isles. The trouble Haskill can get into while I'm gone simply boggles the mind...",
+    "Do you mind? I'm busy doing the fishstick. It's a very delicate state of mind!",
+    "The butler did it! Or is it the advisor? Whoever that man behind the throne was.",
+    "Ah, now this is a sad path. Pelagius hated and feared many things. Assassins, wild dogs, the undead, pumpernickel...",
+    "Um... We're not talking about Barbas, are we? Clavicus Vile's... dog? Oohh... awkward.",
+    "You know, I was there for that whole sordid affair. Marvelous time! Butterflies, blood, a Fox, a severed head... Oh, and the cheese! To die for.",
+    "<img style=\"max-width: 64px\" src=\"https://cdn.discordapp.com/emojis/934113805670170714.webp?quality=lossless\" alt=\"Nexus Mods Mug\" decoding=\"async\" fetchpriority=\"low\" loading=\"lazy\" />"
+]
 
 window.onload = () => {
     var toSetText = possibilities_conditionalized[Math.round(Math.random() * (possibilities_conditionalized.length - 1))];
