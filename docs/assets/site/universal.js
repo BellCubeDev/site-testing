87cf02afd245245d664b1983135aa291f6424b14
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2022-05-13 09:30:19 -0400

    Standardization of functions!

--- docs/assets/site/universal.js
+++ docs/assets/site/universal.js
@@ -11,7 +11,7 @@ window.onload = init;
 
 /**
     * @param {HTMLElement} element
-    */
+*/
 var BellCubicDetails = function BellCubicDetails(element) {
     this.element_ = element;
     this.init();
@@ -24,29 +24,23 @@ window['BellCubicDetails'] = BellCubicDetails;
     * @public
     */
 BellCubicDetails.prototype.toggle = function () {
-    console.log('[BCD-DETAILS] toggle() called on ',this)
+    //console.log('[BCD-SUMMARY] toggle() called on ',this)
     if (this.element_.classList.contains('is-open')) {
-        BellCubicDetails.prototype.close();
+        this.close();
     } else {
-        BellCubicDetails.prototype.open();
+        this.open();
     }
 };
 BellCubicDetails.prototype['toggle'] = BellCubicDetails.prototype.toggle;
 
-BellCubicDetails.prototype.onMouseUp_ = BellCubicDetails.prototype.toggle;
-
 /**
-    * Re-evaluate the state of the collapseable menu, preforming the appropriate action.
+    * Re-evaluate the toggle menu's current state.
     *
     * @public
     */
 BellCubicDetails.prototype.reEval = function () {
-    console.log('[BCD-DETAILS] reEval() called on ',this)
-    if (this.element_.classList.contains('is-open')) {
-        this.open();
-    } else {
-        this.close();
-    }
+    //console.log('[BCD-SUMMARY] reEval() called on ',this)
+    if (this.element_.classList.contains('is-open') || this.header.classList.contains('is-open') ) {this.open()} else {this.close()}
 };
 BellCubicDetails.prototype['reEval'] = BellCubicDetails.prototype.reEval;
 
@@ -56,32 +50,39 @@ BellCubicDetails.prototype['reEval'] = BellCubicDetails.prototype.reEval;
     * @public
     */
 BellCubicDetails.prototype.open = function () {
-    console.log('[BCD-DETAILS] open() called on ',this)
-    this.element_.style["margin-top"] = `0px;`;
+    this.element_Child.style["margin-top"] = `0px`;
     this.element_.classList.add('is-open')
     this.header.classList.add('is-open')
 };
 BellCubicDetails.prototype['open'] = BellCubicDetails.prototype.open;
+
 /**
     * Close the collapseable menu.
+    *
     * @public
-*/
+    */
 BellCubicDetails.prototype.close = function () {
-    console.log('[BCD-DETAILS] close() called on ',this)
-    this.element_.style["margin-top"] = `-${this.element_.offsetHeight}px;`;
+    this.element_Child.style["margin-top"] = `-${this.element_.offsetHeight}px`;
     this.element_.classList.remove('is-open')
     this.header.classList.remove('is-open')
 };
 BellCubicDetails.prototype['close'] = BellCubicDetails.prototype.close;
+
+BellCubicDetails.prototype.header = null
+BellCubicDetails.prototype['header'] = BellCubicDetails.prototype.header;
+
+BellCubicDetails.prototype.element_Child = null
+BellCubicDetails.prototype['element_Child'] = BellCubicDetails.prototype.element_Child;
 
 /**
     * Initialize element.
 */
 BellCubicDetails.prototype.init = function () {
     if (this.element_) {
-        console.log(this.element_)
+        //console.log(this.element_)
         this.element_.innerHTML = `<div class="bcd-details_inner">${this.element_.innerHTML}</div>`;
-        this["header"] = this.element_.ownerDocument.querySelector(`.bcd-summary[for="${this.element_.id}"`);
+        this.header = this.element_.ownerDocument.querySelector(`.bcd-summary[for="${this.element_.id}"`);
+        this.element_Child = this.element_.getElementsByClassName('bcd-details_inner')[0];
         this.reEval()
         this.element_.classList.add('initialized');
     }
@@ -102,7 +103,7 @@ window['BellCubicSummary'] = BellCubicSummary;
     * @public
     */
 BellCubicSummary.prototype.toggle = function () {
-    console.log('[BCD-SUMMARY] toggle() called on ',this)
+    //console.log('[BCD-SUMMARY] toggle() called on ',this)
     if (this.for.classList.contains('is-open')) {
         this.close();
     } else {
@@ -119,8 +120,8 @@ BellCubicSummary.prototype.onMouseUp_ = BellCubicSummary.prototype.toggle;
     * @public
     */
 BellCubicSummary.prototype.reEval = function () {
-    console.log('[BCD-SUMMARY] reEval() called on ',this)
-    if (this.for.classList.contains('is-open')) {this.open()} else {this.close()}
+    //console.log('[BCD-SUMMARY] reEval() called on ',this)
+    if (this.for.classList.contains('is-open') || this.element_.classList.contains('is-open') ) {this.open()} else {this.close()}
 };
 BellCubicSummary.prototype['reEval'] = BellCubicSummary.prototype.reEval;
 
@@ -149,6 +150,9 @@ BellCubicSummary.prototype.close = function () {
 BellCubicSummary.prototype.for = null
 BellCubicSummary.prototype['for'] = BellCubicSummary.prototype.for;
 
+BellCubicSummary.prototype.forChild = null
+BellCubicSummary.prototype['forChild'] = BellCubicSummary.prototype.forChild;
+
 /**
     * Initialize element.
     */
@@ -159,7 +163,8 @@ BellCubicSummary.prototype.init = function () {
         this.for = this.element_.ownerDocument.getElementById(this.element_.getAttribute('for'));
         this.forChild = this.for.getElementsByClassName('bcd-details_inner')[0];
         this.element_.classList.add('initialized');
-        // this.for.reEval() is handled in BellCubicDetails
+
+        this.reEval()
     }
 };
 
@@ -373,15 +378,15 @@ function init() {
     // Still in 'window.onload'
 
     var toSetText = possibilities_conditionalized[Math.round(Math.random() * (possibilities_conditionalized.length - 1))];
-    //console.log(`[BCD-RANDOM-TEXT] Text to set: ${JSON.stringify(toSetText)}`);
+    ////console.log(`[BCD-RANDOM-TEXT] Text to set: ${JSON.stringify(toSetText)}`);
 
     // Check condition
     if (checkCondition(toSetText[0])) {
         document.getElementById("randomized-text-field").innerHTML = toSetText[1];
-        //console.log(`[BCD-RANDOM-TEXT] Condition passed. Using conditionalized text.`);
+        ////console.log(`[BCD-RANDOM-TEXT] Condition passed. Using conditionalized text.`);
     } else {
         document.getElementById("randomized-text-field").innerHTML = possibilities_Generic[Math.round(Math.random() * (possibilities_Generic.length - 1))];
-        //console.log(`[BCD-RANDOM-TEXT] Condition failed. Using generic text.`);
+        ////console.log(`[BCD-RANDOM-TEXT] Condition failed. Using generic text.`);
     }
 };
 
@@ -389,24 +394,24 @@ function checkCondition(condition) {
     try {
 
         // Random
-        //console.log(`[BCD-RANDOM-TEXT] Checking random condition`);
+        ////console.log(`[BCD-RANDOM-TEXT] Checking random condition`);
         var rand = Math.random();
         if (tryForJSON(condition, "random")) {
-            //console.log(`[BCD-RANDOM-TEXT] Is random ${rand} <= ${condition.random}?`);
+            ////console.log(`[BCD-RANDOM-TEXT] Is random ${rand} <= ${condition.random}?`);
             if (rand > parseFloat(condition.random)) {
                 return false;
             }
         }
 
         // Time
-        //console.log(`[BCD-RANDOM-TEXT] Checking time condition`);
+        ////console.log(`[BCD-RANDOM-TEXT] Checking time condition`);
         if (tryForJSON(condition, "time")) {
             var time = new Date();
 
             var currentTime = time.getHours() * 60 + time.getMinutes();
             var conditionTime = [condition.time[0][0] * 60 + condition.time[0][1], condition.time[1][0] * 60 + condition.time[1][1]];
 
-            //console.log(`[BCD-RANDOM-TEXT] is ${currentTime} between ${conditionTime[0]} and ${conditionTime[1]}?`);
+            ////console.log(`[BCD-RANDOM-TEXT] is ${currentTime} between ${conditionTime[0]} and ${conditionTime[1]}?`);
 
             if (!(currentTime >= conditionTime[0] && currentTime <= conditionTime[1])) {
                 return false;
@@ -414,25 +419,25 @@ function checkCondition(condition) {
         }
 
         // Date
-        //console.log(`[BCD-RANDOM-TEXT] Checking date condition`);
+        ////console.log(`[BCD-RANDOM-TEXT] Checking date condition`);
         if (tryForJSON(condition, "date")) {
             var date = new Date();
             var currentDate = [date.getMonth() + 1, date.getDate(), date.getFullYear()];
 
-            //console.log(`[BCD-RANDOM-TEXT] is ${currentDate.join('/')} between ${condition.date[0].join('/')} and ${condition.date[1].join('/')}?`);
+            ////console.log(`[BCD-RANDOM-TEXT] is ${currentDate.join('/')} between ${condition.date[0].join('/')} and ${condition.date[1].join('/')}?`);
 
             if (!(
                     (currentDate[2] > condition.date[0][2]) ||
                     (currentDate[2] == condition.date[0][2] && currentDate[0] > condition.date[0][0]) ||
                     (currentDate[2] == condition.date[0][2] && currentDate[0] == condition.date[0][0] && currentDate[1] >= condition.date[0][1])
                 )) {
-                //console.log(`[BCD-RANDOM-TEXT] Date is not above minimum`);
+                ////console.log(`[BCD-RANDOM-TEXT] Date is not above minimum`);
                 return false
             }
 
             // And...
             if (condition.date[1] != [0, 0, 0]) {
-                //console.log(`[BCD-RANDOM-TEXT] Date has a maximum`);
+                ////console.log(`[BCD-RANDOM-TEXT] Date has a maximum`);
 
                 if (!(
                         (currentDate[2] < condition.date[1][2]) ||
@@ -445,7 +450,7 @@ function checkCondition(condition) {
         }
         // And if any of that threw an error,
     } catch (err) {
-        //console.log(`[BCD-RANDOM-TEXT] {checkCondition(${condition})} Error - ${err.name}\n==================\n${err.stack}\n==================`);
+        ////console.log(`[BCD-RANDOM-TEXT] {checkCondition(${condition})} Error - ${err.name}\n==================\n${err.stack}\n==================`);
         return false
     }
 
@@ -454,18 +459,15 @@ function checkCondition(condition) {
 }
 
 function tryForJSON(aJSON, key) {
-    //console.log(`[BCD-RANDOM-TEXT] Checking ${JSON.stringify(aJSON)} for ${key}`);
+    ////console.log(`[BCD-RANDOM-TEXT] Checking ${JSON.stringify(aJSON)} for ${key}`);
     try {
         if (typeof aJSON[key] === "undefined") {
             return false;
         }
     } catch (err) {
-        //console.log(`[BCD-RANDOM-TEXT] {tryForJSON(${aJSON}, ${key})} Error - ${err.name}\n==================\n${err.stack}\n==================`);
+        ////console.log(`[BCD-RANDOM-TEXT] {tryForJSON(${aJSON}, ${key})} Error - ${err.name}\n==================\n${err.stack}\n==================`);
         return false
     }
     return true;
 }
-
-
-
 
