655e27ebf7ce64403cd7e11012d1e5eaa3867a1b
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2022-11-06 19:38:43 -0500

    Better TSC Watch issue handling

--- .vscode/watch.bat
+++ .vscode/watch.bat
@@ -28,8 +28,8 @@ set "doInlineSources=true"
 :watch
 
 title TypeScript Compiler
-cd %~dp0\..\site\
-call npx tsc-watch --build --verbose --onCompilationStarted "title TypeScript Compiler && call robocopy src\ ..\_generated\ts_out\ /s /xf *.js *.ts" --onCompilationComplete "title TypeScript Compiler && node minify.mjs && title TypeScript Compiler" tsconfig.json
+cd %~dp0\..\
+call npx tsc-watch --build --verbose --onCompilationStarted "title TypeScript Compiler && call robocopy site\src\ _generated\ts_out\ /s /xf *.js *.ts" --onCompilationComplete "title TypeScript Compiler && cd site\ && node minify.mjs && cd .. && title TypeScript Compiler" site\tsconfig.json
 
 goto :watch
 exit /b 1
