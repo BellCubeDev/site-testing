1d4ce764c567df1c723aab298acece6e10002fee
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2022-12-10 09:09:05 -0500

    MORE debug logging!

--- site/minify.mjs
+++ site/minify.mjs
@@ -94,19 +94,19 @@ async function evalFilesInDir(dirPath) {
     @param {string} thisPath the path of the file or directory to evaluate
 */
 async function evalFileOrDir(thisPath) {
-    console.log('Evaluating file or directory:', thisPath);
+    console.log('DEBUG: Evaluating file or directory:', thisPath);
     //console.log('Evaluating file or directory:', thisPath);
     if (path.basename(thisPath).startsWith('_')) return;
 
     const stat = await afs.lstat(thisPath);
     if (stat.isDirectory()) {
-        console.log('Evaluating directory:', thisPath);
+        console.log('DEBUG: Evaluating directory:', thisPath);
         return evalFilesInDir(thisPath);
     }
 
     const [,isSassDir,isOriginal, isMinified, ext] = thisPath.match(/(sass_modules)|(?:\.(original))?(?:\.(min))?\.([^.]+)$/) || [];
     //console.log('original:', original, 'minified:', minified, 'ext:', ext);
-    console.log('Evaluating file with...', JSON.stringify({path: thisPath, isSassDir, isOriginal, isMinified, ext}, undefined, 2));
+    console.log('DEBUG: Evaluating file with...', JSON.stringify({path: thisPath, isSassDir, isOriginal, isMinified, ext}, undefined, 2));
     if (isSassDir || isOriginal || isMinified || !ext) return;
 
     switch (ext) {
@@ -119,6 +119,7 @@ async function evalFileOrDir(thisPath) {
     @param {string} filePath
 */
 async function minifyJSFile(filePath) {
+    console.log('DEBUG: Minifying JS file:', filePath);
     if ((filePath.endsWith('.min.js') && !filePath.includes('highlight_js')) || filePath.endsWith('.original.js')) return;
 
     // Check if we've already processed this file
