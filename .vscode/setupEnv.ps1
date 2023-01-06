a3df41208a1c64d877f627c6dec360146666ee67
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2023-01-05 20:46:21 -0500

    Minor workspace maintenance

--- .vscode/setupEnv.ps1
+++ .vscode/setupEnv.ps1
@@ -7,19 +7,7 @@ Write-Host
 try {
     npm install
 } catch {
-    Write-Host "NPM's package install task failed."
-    Write-Host $_
-    exit 1
-}
-
-Write-Host
-Write-Host
-Write-Host "Installing Ruby gem dependencies..."
-Write-Host
-try {
-    bundle install
-} catch {
-    Write-Host "Bundlr's package install task failed." -ForegroundColor cyan
+    Write-Host "NPM's package install task failed." -ForegroundColor red
     Write-Host $_
     exit 1
 }
@@ -27,7 +15,19 @@ try {
 try {
     npm install -g typescript tsc-watch
 } catch {
-    Write-Host "Installing TypeScript and TSC-Watch globally failed." -ForegroundColor cyan
+    Write-Host "Installing TypeScript and TSC-Watch globally failed." -ForegroundColor red
+    Write-Host $_
+    exit 1
+}
+
+Write-Host
+Write-Host
+Write-Host "Installing Ruby gem dependencies..." -ForegroundColor cyan
+Write-Host
+try {
+    bundle install
+} catch {
+    Write-Host "Bundlr's package install task failed." -ForegroundColor red
     Write-Host $_
     exit 1
 }
--- site/untyped-modules.d.ts
+++ site/untyped-modules.d.ts
@@ -1,3 +1,9 @@
+/** Definitions for modules that are not typed or have incorrect/insufficient types
+ *
+ * @author BellCubeDev
+ */
+
+/** I've defined my own type definitions here both because the existing definitions do not fit my needs and because I've technically slightly modified the package via the minifier */
 export interface prettyData {
     /** I've no idea why they did this instead of just exporting `pd` as the default, but hey */
     pd: {
@@ -22,6 +28,7 @@ export interface prettyData {
     }
 }
 
+/** I've defined my own type definitions here both because the existing definitions do not fit my needs and because I've technically slightly modified the package via the minifier */
 export declare module 'included_node_modules/pretty-data/pretty-data' {
     export default prettyData;
 }
