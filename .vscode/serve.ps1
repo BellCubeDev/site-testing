1cd400af58f5564fcf50ea2dcb8f6f3cea09c8c7
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2023-03-17 06:03:49 -0400

    Color! (and the world's most minor SEO)

--- .vscode/serve.ps1
+++ .vscode/serve.ps1
@@ -10,7 +10,7 @@ while ($true) {
     try{
         bundle exec jekyll serve --config _config.yml --host 0.0.0.0 --source ..\_generated\ts_out\ --destination ..\_generated\_jekyll-out\ --verbose --unpublished --safe --trace
     } catch {}
-    
+
     Write-Host "Jekyll serve failed. Retrying in 10 seconds..." -ForegroundColor red
     Write-Host
     Write-Host
@@ -19,5 +19,5 @@ while ($true) {
     Write-Host
     Write-Host
 
-    timeout /t 10 >nul
+    timeout /t 10 | out-null
 }
